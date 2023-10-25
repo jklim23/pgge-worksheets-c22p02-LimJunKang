a3be438b7070bf3bed03b11681c8a8d1866a4351
jklim23 <junkang2233@gmail.com>
2023-10-25 14:05:27 +0800

    added stuff for jumping

--- PGGE worksheet 1/Assets/Scripts/PlayerMovement.cs
+++ PGGE worksheet 1/Assets/Scripts/PlayerMovement.cs
@@ -6,34 +6,59 @@ using TMPro;
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController PlayerController;
+
     private float HInput, VInput;
-    private bool JumpInput,SprintInput;
-    private float RunSpeed, JumpHeight,SprintSpeed, gravity;
+    private float RunSpeed = 12f;
+    private float JumpHeight = 10f;
+    private float SprintSpeed = 24f;
+    private float gravity = -5f;
+    private float GroundDistance = 0.4f;
+
+    
+
     private Vector3 MovementDirection, Velocity;
+    public Transform GroundCheck;
+    public LayerMask GroundMask;
+
+    private bool JumpInput, SprintInput;
+    private bool IsGrounded;
     private void Start()
     {
         PlayerController = GetComponent<CharacterController>();
-        RunSpeed = 12f;
-        SprintSpeed = 24f;
-        JumpHeight = 10f;
-        gravity = -5f;   
+     
+        
+          
+
         JumpInput = Input.GetKeyDown(KeyCode.Space);
         SprintInput = Input.GetKeyDown(KeyCode.LeftShift);
     }
     private void Update()
     {
+        IsGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
+
         HInput = Input.GetAxis("Horizontal");
         VInput = Input.GetAxis("Vertical");
         
         //move charactor left right back and forth 
-        MovementDirection = transform.right * HInput + transform.forward * VInput;
-        PlayerController.Move(MovementDirection * RunSpeed * Time.deltaTime);
+        MovementDirection = transform.right * HInput + transform.forward * VInput; 
+        if (SprintInput)
+        {
+            PlayerController.Move(MovementDirection * SprintSpeed * Time.deltaTime);
+
+        }
+        else
+        {
+            PlayerController.Move(MovementDirection * RunSpeed * Time.deltaTime);
+
+        }
+       
 
         //gravity effect 
         Velocity.y += gravity * Time.deltaTime;
         PlayerController.Move(Velocity  * Time.deltaTime);
 
         //JumpFunction();
+        
     }
 
     //private void JumpFunction()
@@ -42,4 +67,8 @@ public class PlayerMovement : MonoBehaviour
     //        PlayerController.Move(MovementDirection * JumpHeight * Time.deltaTime);
     //    }
     //}
+    private void SprintFunction()
+    {
+        
+    }
 }
