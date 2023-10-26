1fddc772d5f7e5b4ef171b4746b80da14bd69510
jklim23 <junkang2233@gmail.com>
2023-10-26 10:18:32 +0800

    Update PlayerMovementAnswer.cs

--- PGGE worksheet 1/Assets/Scripts/PlayerMovementAnswer.cs
+++ PGGE worksheet 1/Assets/Scripts/PlayerMovementAnswer.cs
@@ -9,7 +9,7 @@ public class PlayerMovementAnswer : MonoBehaviour
     public CharacterController mCharacterController;
     public Animator mAnimator;
     public float mWalkSpeed = 1.0f;
-    public float mRotationSpeed = 50.0f;
+    public float mRotationSpeed = 100.0f;
    
 void Start()
     {
@@ -27,13 +27,13 @@ void Update()
 
         if (mAnimator == null) return;
 
-        transform.Rotate(0.0f, hInput * mRotationSpeed * Time.deltaTime,
-        0.0f);
-        Vector3 forward =
-        transform.TransformDirection(Vector3.forward).normalized;
+        transform.Rotate(0.0f, hInput * mRotationSpeed * Time.deltaTime, 0f);
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward).normalized;
         forward.y = 0.0f;
-        mCharacterController.Move(forward * vInput * speed *
-        Time.deltaTime);
+
+        mCharacterController.Move(forward * vInput * speed * Time.deltaTime);
+
         mAnimator.SetFloat("PosX", 0);
         mAnimator.SetFloat("PosZ", vInput * speed / 2.0f * mWalkSpeed);
     }
